64c0938ae6d2f139102e4c356dae4f21d583ae74
indranilokg@gmail.com <indranil.jha@okta.com>
2026-03-14 14:16:38 -0500

    fix

--- okta_agent_proxy/main.py
+++ okta_agent_proxy/main.py
@@ -560,7 +560,7 @@ def run(
                             "jsonrpc": "2.0",
                             "id": body.get("id"),
                             "result": {
-                                "protocolVersion": "2024-11-05",
+                                "protocolVersion": "2025-11-25",
                                 "capabilities": {
                                     "tools": {},
                                     "resources": {
